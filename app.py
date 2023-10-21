1023687e6409caa39f1ac09d077c9801a29fcdd6
Arthur Gao <arthurgao@revelstoke.localdomain>
2023-10-21 16:11:46 -0400

    Added clearing of chat

--- app.py
+++ app.py
@@ -4,12 +4,12 @@ from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
 # Put your key here
-openai.api_key = "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"
+openai.api_key = "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"
 
 # TODO: Test and modify variations of this prompt
 # I want variations of the prompt that make it adhere to the script a bit less
-# Initialize history with a system context prompt
-history = [{
+# Initialize HISTORY with a system context prompt
+HISTORY = [{
     "role": "system",
     "content": '''You are a helpful, compassionate, assistant and well listening companion. Try to be conversational. The user has a major stressor in their life and you should assist them in guiding them through the stressor.
                 Help them work through their stressors by performing a Reflective Questioning Activity. Some guides on questions to ask are:
@@ -34,10 +34,10 @@ history = [{
 # Temperature < 1 seemed to be very strict in following the guidelines
 # Temperature range 0 - 2
 # TODO: Test effects of different temperature, top_p, frequency_penalty, and presence_penalty
-def get_completion(prompt, model="gpt-4"):
-    # Append the users input to the context history
-    history.append({"role": "user", "content": prompt})
-    messages = history
+def get_completion(prompt, model="gpt-3.5-turbo"):
+    # Append the users input to the context HISTORY
+    HISTORY.append({"role": "user", "content": prompt})
+    messages = HISTORY
     print(messages)
 
     response = openai.ChatCompletion.create(
@@ -50,7 +50,7 @@ def get_completion(prompt, model="gpt-4"):
         presence_penalty=0.10
     )
     print(response)
-    history.append({"role": "assistant", "content": response.choices[0].message["content"]})
+    HISTORY.append({"role": "assistant", "content": response.choices[0].message["content"]})
     return response.choices[0].message["content"]
 
 
@@ -65,6 +65,33 @@ def get_bot_response():
     response = get_completion(userText)
     # return str(bot.get_response(userText))
     return response
+
+
+@app.route("/clear", methods=['POST'])
+def clear_chat():
+    global HISTORY
+    HISTORY = [{
+        "role": "system",
+        "content": '''You are a helpful, compassionate, assistant and well listening companion. Try to be conversational. The user has a major stressor in their life and you should assist them in guiding them through the stressor.
+                    Help them work through their stressors by performing a Reflective Questioning Activity. Some guides on questions to ask are:
+                    What’s the situation? Feel free to explain it in as much detail as you’d like.
+                    What part of the situation is most troubling?
+                    What are you thinking to yourself?
+                    What thought is the most troubling?
+                    What do you feel when you think this?
+                    When you have these feelings, what actions do you take? What actions do you avoid?
+                    Ask the user to re-type a summary of the situation in the following format: Trigger: Thought: Feeling: Behavior
+                    Consider whether the trigger truly justifies this type of thinking. Explain below.
+                    If you were to explore an alternative line of thinking, how would you do it?
+                '''
+    },
+        {
+            "role": "assistant",
+            "content": "Think of a particular situation where you felt stressed or had a negative emotion, which you can try to reflect on as you go through this activity. It could be a current situation, one in the past, or one you anticipate in the future."
+        }
+    ]
+
+    return "True"
 
 
 if __name__ == '__main__':
