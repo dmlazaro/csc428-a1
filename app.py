07dcfec70ca1ef88473bfe6287d463386c3f705d
Arthur Gao <arthurgao@revelstoke.localdomain>
2023-10-21 15:22:53 -0400

    added tuning to the model

--- app.py
+++ app.py
@@ -4,35 +4,37 @@ from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
 # Put your key here
-openai.api_key = ""
+openai.api_key = "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"
 
 # TODO: Test and modify variations of this prompt
 # I want variations of the prompt that make it adhere to the script a bit less
 # Initialize history with a system context prompt
 history = [{
     "role": "system",
-    "content": '''You are a helpful assistant and well listening companion. The user has a major stressor in their life and you should assist them in guiding them through the stressor. The user will be answering:
-                “Think of a particular situation where you felt stressed or had a negative emotion, which you can try to reflect on as you go through this activity. It could be a current situation, one in the past, or one you anticipate in the future.”
-                
+    "content": '''You are a helpful, compassionate, assistant and well listening companion. Try to be conversational. The user has a major stressor in their life and you should assist them in guiding them through the stressor.
                 Help them work through their stressors by performing a Reflective Questioning Activity. Some guides on questions to ask are:
-
                 What’s the situation? Feel free to explain it in as much detail as you’d like.
                 What part of the situation is most troubling?
                 What are you thinking to yourself?
                 What thought is the most troubling?
                 What do you feel when you think this?
                 When you have these feelings, what actions do you take? What actions do you avoid?
-                Re-type the summary of the situation in the following format: Trigger: Thought: Feeling: Behavior
+                Ask the user to re-type a summary of the situation in the following format: Trigger: Thought: Feeling: Behavior
                 Consider whether the trigger truly justifies this type of thinking. Explain below.
                 If you were to explore an alternative line of thinking, how would you do it?
             '''
-}]
+},
+    {
+        "role": "assistant",
+        "content": "Think of a particular situation where you felt stressed or had a negative emotion, which you can try to reflect on as you go through this activity. It could be a current situation, one in the past, or one you anticipate in the future."
+    }
+]
 
 
 # Temperature < 1 seemed to be very strict in following the guidelines
 # Temperature range 0 - 2
 # TODO: Test effects of different temperature, top_p, frequency_penalty, and presence_penalty
-def get_completion(prompt, model="gpt-3.5-turbo"):
+def get_completion(prompt, model="gpt-4"):
     # Append the users input to the context history
     history.append({"role": "user", "content": prompt})
     messages = history
@@ -41,11 +43,11 @@ def get_completion(prompt, model="gpt-3.5-turbo"):
     response = openai.ChatCompletion.create(
         model=model,
         messages=messages,
-        temperature=1.50,
+        temperature=1,
         max_tokens=256,
         top_p=1,
-        frequency_penalty=0.50,
-        presence_penalty=0.25
+        frequency_penalty=0.10,
+        presence_penalty=0.10
     )
     print(response)
     history.append({"role": "assistant", "content": response.choices[0].message["content"]})
